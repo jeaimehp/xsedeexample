423c324e465804a7b006f76232334f2c5d28a5ed
Jeaime Powell <jhpowell@login2.stampede2.tacc.utexas.edu>
2019-02-07 14:25:07 -0600

    corrected allocation

--- example_submission.bash
+++ example_submission.bash
@@ -3,7 +3,8 @@
 #SBATCH -o test.o%j        # output and error file name (%j expands to jobID)
 #SBATCH -n 1               # total number  requested
 #SBATCH -N 1               # total number nodes requested
-#SBATCH -p TG-ASC190007    # queue (partition) -- normal, development, etc.
+#SBATCH -p development     # queue (partition) -- normal, development, etc.
+#SBATCH -A TG-ASC190007   # Allocation/Project name 
 #SBATCH -t 00:00:45          # run time (hh:mm:ss) - 1.5 hours
 #SBATCH --mail-user=jpowell@tacc.utexas.edu
 #SBATCH --mail-type=begin  # email me when the job starts
