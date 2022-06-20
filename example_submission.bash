a4a651791228b20c92f52480594128fcd6c76ed4
Je'aime Powell <jeaimehp@users.noreply.github.com>
2022-06-20 18:13:59 -0500

    updated project for Bioinformatics-Works

--- example_submission.bash
+++ example_submission.bash
@@ -4,7 +4,7 @@
 #SBATCH -n 1               # total number  requested
 #SBATCH -N 1               # total number nodes requested
 #SBATCH -p development     # queue (partition) -- normal, development, etc.
-#SBATCH -A TG-ASC190007   # Allocation/Project name 
+#SBATCH -A Bioinformatics-Works   # Allocation/Project name 
 #SBATCH -t 00:00:45          # run time (hh:mm:ss) - 1.5 hours
 #SBATCH --mail-user=jpowell@tacc.utexas.edu
 #SBATCH --mail-type=begin  # email me when the job starts
